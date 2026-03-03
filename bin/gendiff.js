66afa51bf1fb7e96adf8b7c0606039fbfe48631b
luisfelipemontoya <luisfelipemontoya88@gmail.com>
2026-03-02 20:16:46 -0500

    Add CLI arguments and format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
